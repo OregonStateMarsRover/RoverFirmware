7493fee4f226aef5d3d7bc80868e0405d7994486
Marshal Horn <kamocat@gmail.com>
2012-11-02 09:49:22 -0700

    Bogie: Sabertooth communication now works.

--- bogie/BogieController.c
+++ bogie/BogieController.c
@@ -4,8 +4,11 @@
 
 #include "BogieController.h"
 
+#define GREEN 0x10;	// green LED
+#define RED 0x20;	// red LED
 
-void bogie_controller_init(void)
+
+void init(void)
 {
 	USART_InitPortStructs();
 	
@@ -18,7 +21,7 @@ void bogie_controller_init(void)
 	/***Motor Driver USART init***/
 		
 	
-	USART_Open(&bogie.motor, 2, USART_BAUD_9600, 10, 10, false, true);
+	USART_Open(&bogie.motor, 2, USART_BAUD_9600, 10, 10, false, false);
 	/***Mainboard USART init***/
 	/*
 	USART_Open(&bogie.bb, 0, USART_BAUD_38400, 10, 10, false, true);
@@ -41,21 +44,53 @@ void bogie_controller_init(void)
 
 int main(void)
 {
-	bogie_controller_init();
+	init();
 
 	int8_t i = 0;
 	int8_t inc = 1;
+	uint8_t flash_mode;
 
+	/* Ramp the drive motor speed up and down,
+	 * to test the motor controllers */
 	while(1) {
 		drive_set( i );
-		i += inc;
-		_delay_ms( 10 );
+		_delay_ms( 40 );
 
 		if( i == 127 ) {
 			inc = -1;
 		} else if( i == -127 ) {
 			inc = 1;
 		}
+		i += inc;
+
+
+		/* Flash the LEDs so we know what should be happening.
+		 * ON means the motor is currently GOING that direction.
+		 * BLINK means the motor is CHANGING TOWARDS that direction.
+		 */
+		flash_mode = (i & 0x80) | (inc & 0x01);
+		switch( flash_mode ) {
+				case 0x01:
+						PORTD.OUTTGL = GREEN;
+						PORTD.OUTCLR = RED;
+						break;
+				case 0x00:
+						PORTD.OUTSET = GREEN;
+						PORTD.OUTTGL = RED;
+						break;
+				case 0x80:
+						PORTD.OUTCLR = GREEN;
+						PORTD.OUTTGL = RED;
+						break;
+				case 0x81:
+						PORTD.OUTTGL = GREEN;
+						PORTD.OUTSET = RED;
+						break;
+		}
+
+
+
+
 	}
 
 	return 0;
