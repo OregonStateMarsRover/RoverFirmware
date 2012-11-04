9bcf8d0bb22baf70985aa8c1934cdbd6d31b7d85
Marshal Horn <kamocat@gmail.com>
2012-11-03 22:14:44 -0700

    Bogie: Added function to change clock frequency. Don't use it unless you also change F_CPU and the UART configs.

--- bogie/BogieCommon.c
+++ bogie/BogieCommon.c
@@ -4,6 +4,23 @@
 
 #include "BogieCommon.h"
 
+/* This function sets the clock speed for the microcontroller.
+ * When you change it, make sure to change F_CPU to match.
+ * Currently, it changes the main clock to the external crystal,
+ * and makes the prescaler 1.
+ * However, the 9600 baud setting in UART.c is probably still set
+ * for the 2Mhz clock, and also needs to be adjusted.
+ */
+void set_clock( void ) {
+	CCP = IOREG;	// enable change of protected registers
+	CTRL = 3;		// use external clock
+
+	CCP = IOREG;	// enable change of protected registers
+	PSCTRL = 0;		// disable prescaler
+
+
+}
+
 uint8_t pid(PIDobject *pid, uint16_t desired, uint16_t actual)
 {
 	uint16_t p_error = desired - actual;
@@ -34,15 +51,6 @@ void pid_init( void ) {
 
 }
 
-/*
-ISR(TCD0_OVF_vect)
-{
- 	PORTD.OUTTGL = 0b00110000;
- 	drive_set(desired_speed); 
- 	actuator_set(pid(&act_pid, desired_angle, get_actuator_pos()));
-}
-*/
-
 
 void parse_command(CommPacket *pkt)
 {
@@ -60,6 +68,14 @@ void reply(CommPacket *pkt) {
 
 	CommSendPacket(&bogie.bb_com, &respPkt);
 }
+
+
+/* How is this going to work if we are using both our timer/counters
+ * for encoders?  We may need to put our optical encoder on TC1, and
+ * split TC0 so we can use half for timing the speed of the drive
+ * motors, and half for timing our PID.
+ * Or, perhaps we could use the real-time counter.
+ */
 /*
 ISR(TCD0_OVF_vect)
 {
