ed56a8734888db6dd6d0fda8819e85327c6f5312
Marshal Horn <kamocat@gmail.com>
2012-11-10 18:00:13 -0800

    Bogie: Clock is now 16Mhz

--- bogie/BogieCommon.c
+++ bogie/BogieCommon.c
@@ -14,14 +14,14 @@
 void set_clock( void ) {
 	// Wait until the oscilator is stable
 	PORTD.DIRSET = 0x20;
-	while( !(OSC.STATUS & 0x40) ) {
+	while( !(OSC.STATUS & 0x08) ) {
 		PORTD.OUTTGL = 0x20;	// toggle red LED
-		OSC.XOSCCTRL = 0b11011011;	// set up oscillator for 16 Mhz
-		OSC.CTRL |= 0x40;	// Start the external clock.
+		OSC.XOSCCTRL = 0b11001011;	// set up oscillator for 16 Mhz
+		OSC.CTRL |= 0x08;	// Start the external clock.
 		_delay_ms(50);
 	}
-	PORTD.OUTCLR = 0x20;	// turn off red LED
-	//OSC.PLLCTRL = 0b11000001;	//Set PLL to use external clock
+	PORTD.OUTSET = 0x20;	// turn off red LED
+	OSC.PLLCTRL = 0b11000001;	//Set PLL to use external clock
 	
 	CCP = 0xD8;	// enable change of protected registers
 	CLK.CTRL = 3;		// use external clock
