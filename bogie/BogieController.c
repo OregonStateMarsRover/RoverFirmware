331d55a91cf698c27a5c08f736e9a1fc208b0e14
Marshal Horn <kamocat@gmail.com>
2012-11-14 11:09:34 -0800

    Bogie: Working on encoders, but need rs485 for feedback and troubleshooting

--- bogie/BogieController.c
+++ bogie/BogieController.c
@@ -3,6 +3,7 @@
  */
 
 #include "BogieController.h"
+#include <stdio.h>
 
 #define GREEN 0x10;	// green LED
 #define RED 0x20;	// red LED
@@ -23,8 +24,8 @@ void init(void)
 	
 	USART_Open(&bogie.motor, 2, USART_BAUD_9600, 10, 10, false, false);
 	/***Mainboard USART init***/
+	USART_Open(&bogie.bb, 0, USART_BAUD_115200, 50, 10, true, false );
 	/*
-	USART_Open(&bogie.bb, 0, USART_BAUD_38400, 10, 10, false, true);
 	CommInterfaceInit(&bogie.bb_com, &bogie.bb);
 	
 	PacketQueueInit2(&bogie.pktQueue, 6, 20, bogie.queuedPackets, bogie.queuedData);
@@ -46,14 +47,23 @@ int main(void)
 {
 	init();
 
+	USART_Write( &bogie.bb, (uint8_t *)"Encoder test\r\n", 14 );
+
 	int8_t i = 0;
 	int8_t inc = 1;
 	uint8_t flash_mode;
 
+	char msg[20];
+	uint8_t msg_len;
+
 	/* Ramp the drive motor speed up and down,
 	 * to test the motor controllers */
 	while(1) {
-		drive_set( i );
+		msg_len = snprintf( msg, 20, "%d, ", get_turn() );
+		if( msg_len > 20 ) msg_len = 20;
+		USART_Write( &bogie.bb, (uint8_t *)msg, msg_len );
+			
+		actuator_set( i );
 		_delay_ms( 40 );
 
 		if( i == 127 ) {
@@ -87,11 +97,6 @@ int main(void)
 						PORTD.OUTSET = RED;
 						break;
 		}
-
-
-
-
 	}
-
 	return 0;
 }
